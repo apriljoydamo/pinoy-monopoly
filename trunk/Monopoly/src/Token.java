cca26a3d4940e6d154593075e9811f5b66065b4e
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-16 09:15:07 +0000

    token Class

---
+++ trunk/Monopoly/src/Token.java
@@ -0,0 +1,46 @@
+
+import javax.swing.JLabel;
+
+
+public class Token extends JLabel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int xLocation = 508;
+	private int yLocation = 508;
+	private int assignedToken;
+	
+	public Token(){
+	}
+	
+	
+	//GETTER AND SETTER
+	public void setxLocation(int xLocation) {
+		this.xLocation = xLocation;
+	}
+	
+	public int getxLocation() {
+		return xLocation;
+	}
+	
+	public void setyLocation(int yLocation) {
+		this.yLocation = yLocation;
+	}
+	
+	public int getyLocation() {
+		return yLocation;
+	}
+
+
+	public void setAssignedToken(int assignedToken) {
+		this.assignedToken = assignedToken;
+	}
+
+
+	public int getAssignedToken() {
+		return assignedToken;
+	}
+		
+
+}
