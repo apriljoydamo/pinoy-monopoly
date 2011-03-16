68d2f20dc96705cfc13efe13a16053306a87f523
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-16 09:13:40 +0000

    Edited Player Class

--- trunk/Monopoly/src/Players.java
+++ trunk/Monopoly/src/Players.java
@@ -1,191 +1,34 @@
+
+
+
 public class Players{
 	
-	private String TokenName;
-	private int StartMoney;
-	static int PlayersPlaying;
+	private int startMoney = 15000;
 	private String playerName;
-	private int token;
-	boolean Multi;
-	public int LabelToken;
+	private Token token;
+	private int tempOrder;
+	private int order = 1;
 	
-	public void chooseSolo(){
-		System.out.println("You choosed SOLO PLAYER.");
-		PlayersPlaying = 1;
-		Multi = false;
-	}
-	
-	public void chooseMulti(){
-		System.out.println("You choose MULTIPLAYER.");
-		Multi = true;
-	}
-	
-	public void EnterNumberPlayers(int NumPlayers){
-		System.out.println("Enter number of Players: ");
-		PlayersPlaying = NumPlayers;
-		System.out.println(PlayersPlaying);
-	}
+	//FUNCTIONs
 
-	public void addPlayerName(String Name){
-			System.out.println("Enter Name: ");
-				setPlayerName(Name);
-				setToken(choosePlayerToken(LabelToken));
-				initializeStartMoney();
-				System.out.println(Name + " as " + TokenName + " has " + StartMoney);
-			
-	}
-	
-	public Players createPlayer(){
+	public Players createPlayer(String Name){
 		Players players = new Players();
-		players.addPlayerName(toString());
+		players.token = new Token();
+		players.setPlayerName(Name);
+		//players.setToken(token);
+		players.setStartMoney(startMoney);
+		//System.out.println(startMoney + " labeltoken " + labelToken);
 		return players;
 	}
 	
-	public int choosePlayerToken(int LabelToken){
-			switch(LabelToken){
-			
-			case 1:
-				TokenName = "Bapor";
-				System.out.println("bapor");
-				break;
-				
-			case 2:
-				TokenName = "Azkal";
-				System.out.println("azkal");
-				break;
-				
-			case 3:
-				TokenName = "Iron";
-				System.out.println("iron");
-				break;
-				
-			case 4:
-				TokenName = "Shoe";
-				System.out.println("shoe");
-				break;
-				
-			case 5:
-				TokenName = "Money Bag";
-				System.out.println("moneybag");
-				break;
-				
-			case 6:
-				TokenName = "Train";
-				System.out.println("train");
-				break;
-				
-			case 7:
-				TokenName = "Thimble";
-				System.out.println("thimble");
-				break;
-				
-			case 8:
-				TokenName = "Man On Horseback";
-				System.out.println("man on horseback");
-				break;
-			
-			}
-		
-		return LabelToken;
-
-	}
-
-	public void initializeStartMoney(){
-				setStartMoney(1500);		
-	}
 	
-	public void moveToken(){
-				
-	}
-		/*if(PlayersPlaying == 1){
-			do{	
-				int c = 0;
-				for(int x = 0; x < 1; x++){
-					c++;
-					System.out.println("PLAYER #1:" + players[x] + " " + "TOKEN:" + TokenName +"");
-					System.out.println("Enter number of steps:");
-					WalkMove = scanner.nextInt();
-					
-					for(int i = 1; i <= WalkMove; i++){
-						System.out.println(i);
-						Thread.sleep(1000);
-					}
-					System.out.println("next!");
-				}
-			}while(PlayersPlaying == 1);
-		}
-		
-		//addPlayer();
-		if(Multi == true){
-			do{
-				int c = 0;
-				for(int x = 0; x < PlayersPlaying; x++){
-					c++;
-					System.out.println("PLAYER #" + c + ":" + players[x] + "  " + "TOKEN:" + token[x] +"");
-					System.out.println("Enter number of steps:");
-					WalkMove = scanner.nextInt();
-					
-					for(int i = 1; i <= WalkMove;i++){
-						System.out.println(i);
-						Thread.sleep(1000);
-					}
-					System.out.println("next!");
-				}
-			}while(Multi == true);
-		}
-		public void run() {
-			while(isRunning){
-				System.out.println("RUN!");
-				if(LabelToken == 1){
-					board.getDogToken().setLocation(board.getDogToken().getX()+1, board.getDogToken().getY()+1);
-					try{
-						Thread.sleep(500);
-					}
-					catch(InterruptedException e){}
-				}
-			}
-		}
-		@SuppressWarnings("deprecation")
-		public void actionPerformed(ActionEvent e){
-			if(board.isRolled == true){
-				t = new Thread(this);
-				isRunning = true;
-				t.start();
-			}else if(board.isRolled == false){
-				t.stop();
-				isRunning = false;
-			}
-			
-		}
-	
-	}
-			
-
-
-public static void main(String[]args) throws InterruptedException{
-	Players players = new Players();
-	System.out.println("Start. Choose Player Type.");
-	players.chooseSolo();
-	System.out.println("Add Player Name.");
-	players.addPlayerName("aj");
-	players.choosePlayerToken(4);
-	System.out.println("List of Players.");
-	players.printPlayers();
-	//players.moveToken();
-	///players.printChosenToken();
-	//players.addPlayer();
-	players.initializeStartMoney();
-	//System.out.println("New set of players.");
-	//players.addPlayerName();
-	//players.printPlayers();
-	
-}}*/
-
+	//GETTER AND SETTER
 	public void setStartMoney(int startMoney) {
-		StartMoney = startMoney;
+		this.startMoney = startMoney;
 	}
 
 	public int getStartMoney() {
-		return StartMoney;
+		return startMoney;
 	}
 
 	public void setPlayerName(String playerName) {
@@ -196,12 +39,36 @@ public static void main(String[]args) throws InterruptedException{
 		return playerName;
 	}
 
-	public void setToken(int token) {
+	public void setToken(Token token) {
 		this.token = token;
+		
 	}
 
-	public int getToken() {
+	public Token getToken() {
 		return token;
+	}
+
+
+	public void setTempOrder(int tempOrder) {
+		this.tempOrder = tempOrder;
+	}
+
+
+	public int getTempOrder() {
+		return tempOrder;
+	}
+
+
+
+
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+
+	public int getOrder() {
+		return order;
 	}
 }
 
