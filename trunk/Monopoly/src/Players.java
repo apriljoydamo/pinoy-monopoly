bc2e481c99c85f3566782049c1007f0dee103b81
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-19 14:18:18 +0000

    same

--- trunk/Monopoly/src/Players.java
+++ trunk/Monopoly/src/Players.java
@@ -1,30 +1,25 @@
 
-
-
 public class Players{
 	
-	private int startMoney = 1500;
+	private int tempOrder, totalSteps;
+	private int order = 1, startMoney = 1500, position = 0;
 	private String playerName;
 	private Token token;
-	private int tempOrder;
-	private int order = 1;
-	private int totalSteps;
-	private int position = 0;
 	
-	//FUNCTIONs
-
+	
+	
+/////////////////////Creating a Player/////////////////////////////////////
 	public Players createPlayer(String Name){
 		Players players = new Players();
 		players.token = new Token();
 		players.setPlayerName(Name);
-		//players.setToken(token);
 		players.setStartMoney(startMoney);
 		//System.out.println(startMoney + " labeltoken " + labelToken);
 		return players;
 	}
 	
 	
-	//GETTER AND SETTER
+/////////////////////GETTER AND SETTER//////////////////////////////////////
 	public void setStartMoney(int startMoney) {
 		this.startMoney = startMoney;
 	}
