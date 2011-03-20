84313ce4325b1e9b0747e1ec8d25f163508fbbed
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-20 09:06:12 +0000

    community chest class

--- trunk/newMonopoly/src/CommunityChest.java
+++ trunk/newMonopoly/src/CommunityChest.java
@@ -3,8 +3,7 @@
 public class CommunityChest extends Block {
 
 	public CommunityChest(String name, int location){
-		this.setName(name);
-		this.setLocation(location);
+		super(name, location);
 		
 	}
 }
