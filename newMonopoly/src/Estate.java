2340e79841f1d548fee627a4a79ddcd376dc4d98
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-20 09:06:33 +0000

    Estate class

--- newMonopoly/src/Estate.java
+++ newMonopoly/src/Estate.java
@@ -1,9 +1,8 @@
 
 public class Estate extends Property{
 
-	public Estate(String name, int owner, int location, int price){
+	public Estate(String name, int location, int price){
 		this.setName(name);
-		this.setOwner(owner);
 		this.setLocation(location);
 		this.setPrice(price);
 	}
