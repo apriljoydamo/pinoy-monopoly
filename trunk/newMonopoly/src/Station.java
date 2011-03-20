d254d4f56d4b3c54367d45f576a34025277c92d5
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-20 09:07:28 +0000

    Station class

--- trunk/newMonopoly/src/Station.java
+++ trunk/newMonopoly/src/Station.java
@@ -1,9 +1,8 @@
 
 public class Station extends Property{
 	
-	public Station(String name, int owner, int location, int price){
+	public Station(String name, int location, int price){
 		this.setName(name);
-		this.setOwner(owner);
 		this.setLocation(location);
 		this.setPrice(price);
 	}
