1a90d759e248af00d3e5009b6d3e0b6726ebca67
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-21 18:22:25 +0000

    added getter and setter

--- trunk/newMonopoly/src/Block.java
+++ trunk/newMonopoly/src/Block.java
@@ -10,9 +10,34 @@ public class Block {
 	private Chance chance;
 	private CommunityChest cChest;
 	
+
 	public Block(String name, int location){
 		this.setName(name);
 		this.setLocation(location);
+	}
+	
+	public Block(Station station) {
+		this.station = station;
+	}
+	
+	public Block(Utilities utilities) {
+		this.utilities = utilities;
+	}
+	
+	public Block(Estate estate) {
+		this.estate = estate;
+	}
+	
+	public Block(Tax tax) {
+		this.tax = tax;
+	}
+	
+	public Block(Chance chance) {
+		this.chance = chance;
+	}
+	
+	public Block(CommunityChest cChest) {
+		this.cChest = cChest;
 	}
 	
 ////////////////////////GETTERS AND SETTERS/////////////////////////////
@@ -23,43 +48,50 @@ public class Block {
 	public int getLocation() {
 		return location;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
 	public String getName() {
 		return name;
 	}
-	public Block(Station station) {
+	
+	public void setStation(Station station){
 		this.station = station;
 	}
 	public Station getStation() {
 		return station;
 	}
-	public Block(Utilities utilities) {
+	
+	public void setUtilities(Utilities utilities){
 		this.utilities = utilities;
 	}
 	public Utilities getUtilities() {
 		return utilities;
 	}
-	public Block(Estate estate) {
+	
+	public void setEstate(Estate estate){
 		this.estate = estate;
 	}
 	public Estate getEstate() {
 		return estate;
 	}
-	public Block(Tax tax) {
+	
+	public void setTax(Tax tax){
 		this.tax = tax;
 	}
 	public Tax getTax() {
 		return tax;
 	}
-	public Block(Chance chance) {
+	
+	public void setChance(Chance chance){
 		this.chance = chance;
 	}
 	public Chance getChance() {
 		return chance;
 	}
-	public Block(CommunityChest cChest) {
+	
+	public void setCommunityChest(CommunityChest cChest){
 		this.cChest = cChest;
 	}
 	public CommunityChest getcChest() {
