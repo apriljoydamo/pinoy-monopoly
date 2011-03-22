067be0164ede09fc6df535b70f13e60b0b154548
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-22 19:39:43 +0000

    Property Class

---
+++ newMonopoly/src/Property.java
@@ -0,0 +1,39 @@
+
+public class Property extends Block{
+	
+	private int price = 0;
+	private String propertyName = null, ownerName = null;
+	private boolean isOwned = false;
+	//private String name;// = super.getBlockName();
+	
+	public Property(String name, int location) {
+		super(name, location);
+		this.setPrice(price);
+	}
+	
+	////////////////////////////GETTERS AND SETTERS/////////////////////
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	public String getPropertyName() {
+		return propertyName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwned(boolean isOwned) {
+		this.isOwned = isOwned;
+	}
+	public boolean isOwned() {
+		return isOwned;
+	}
+}
