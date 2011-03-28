784c9db3407f765d0f29666a7927b5f691af2d2b
lhaine.nicolas@gmail.com <lhaine.nicolas@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-28 13:55:25 +0000

    Cchest methods. not yet finish. random working.

--- newMonopoly/src/CommunityChest.java
+++ newMonopoly/src/CommunityChest.java
@@ -1,76 +1,105 @@
-
+import java.util.Random;
 
 public class CommunityChest{
-	private int communityChestRandom;
 	
+	Players playerOrder[] = new Players[8];
+	private int x = 0;
 	
-	public void shuffleCommunityChest(int chest){
-		
-		switch(chest){
-        case 1:
-                System.out.println("Bank error in your favour. Collect P200.");
-                break;
-        case 2:
-        		System.out.println("Pay hospital P100.");
-                break;
-        case 3:
-        		System.out.println("You have won second price in beauty contest. Collect P10.");
-                break;
-        case 4:
-        		System.out.println("Income tax refund. Collect P20.");
-                break;
-        case 5:
-        		System.out.println("Pay a P10 fine or take a CHANCE.");
-                break;
-        case 6:
-        		System.out.println("Pay your insurance premium P50.");
-        		break;
-        case 7:
-    			System.out.println("From sale of stock you get P50.");
-    			break;
-        case 8:
-	    		System.out.println("Recieve interest on 7% preference shares P25.");
-	    		break;
-        case 9:
-	    		System.out.println("Advance to GO.");
-	    		break;
-        case 10:
-	    		System.out.println("Go back to Old Kent Road.");
-	    		break;
-        case 11:
-	    		System.out.println("Get out of Jail Key.");
-	    		break;
-        case 12:
-	    		System.out.println("It is your birthday. Collect P10 from each player.");
-	    		break;
-        case 13:
-	    		System.out.println("Doctor's fee. Pay P50.");
-	    		break;
-        case 14:
-	    		System.out.println("Annuity matures. Collect P100.");
-	    		break;
-        case 15:
-	    		System.out.println("GO TO JAIL.");
-	    		break;
-        case 16:
-	    		System.out.println("You inherit P100.");
-	    		break;
-	    		
-		}
+	private int cChestRandom;
+	
+	Random random = new Random();
+	
+	public int shuffleCchest(){
+		Random random = new Random();
+		setcChestRandom(random.nextInt(16)+ 1);
+		System.out.println("Community Chest Shuffle Result " + cChestRandom);
+		return getcChestRandom();
 	}
 	
+	public void bankError(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 200);
+		System.out.println("Bank error in your favour. Collect P200.");
+	}
 	
-
+	public void payHospital(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() - 100);
+		System.out.println("Pay hospital P100.");
+	}
+	
+	public void wonBeautyContest(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 10);
+		System.out.println("You have won second price in beauty contest. Collect P10.");
+	}
+	
+	public void taxRefund(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 20);
+		System.out.println("Income tax refund. Collect P20.");
+	}
+	
+	public void payOrChance(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() - 10);
+		System.out.println("Pay a P10 fine or take a CHANCE.");
+	}
+	
+	public void payInsurance(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() - 50);
+		System.out.println("Pay your insurance premium P50.");
+	}
+	
+	public void saleOfStock(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 50);
+		System.out.println("From sale of stock you get P50.");
+	}
+	
+	public void recieveInterest(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 25);
+		System.out.println("Recieve interest on 7% preference shares P25.");
+	}
+	
+	public void advanceToGo(){
+		System.out.println("Advance to GO.");
+	}
+	
+	public void goBackToOldKentRoad(){
+		System.out.println("Go back to Old Kent Road.");
+	}
+	
+	public void jailFreeKey(){
+		System.out.println("Get out of Jail Key.");
+	}
+	
+	public void yourBirthday(){
+		System.out.println("It is your birthday. Collect P10 from each player.");
+	}
+	
+	public void doctorsFee(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() - 50);
+		System.out.println("Doctor's fee. Pay P50.");
+	}
+	
+	public void annuityMatures(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 100);
+		System.out.println("Annuity matures. Collect P100.");
+	}
+	
+	public void goToJail(){
+		System.out.println("GO TO JAIL.");
+	}
+	
+	public void inheritMoney(){
+//		playerOrder[x].setStartMoney(playerOrder[x].getStartMoney() + 100);
+		System.out.println("You inherit P100.");
+	}
 //////////GETTER and SETTER//////////
 	
 	
-	public void setCommunityChestRandom(int ccRandom) {
-		this.communityChestRandom = ccRandom;
+	public void setcChestRandom(int cChestRandom) {
+		this.cChestRandom = cChestRandom;
 	}
 
 
-	public int getCommunityChestRandom() {
-		return communityChestRandom;
+	public int getcChestRandom() {
+		return cChestRandom;
 	}
 
 }
