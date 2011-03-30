b9bc0b1b4a4daba1e2b623266199968009ab0b26
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-30 16:24:20 +0000

    for rent

--- trunk/newMonopoly/src/Station.java
+++ trunk/newMonopoly/src/Station.java
@@ -1,10 +1,11 @@
 
 public class Station extends Property{
 	
-	public Station(String name, int location, int price, boolean isOwned){
+	public Station(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
 		this.setOwned(isOwned);
+		this.setTitledeed(titledeed);
 	}
 }
