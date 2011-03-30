f4913017b17edf07160719c6e79d8d71b958c3de
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-30 16:45:34 +0000

    for rent

--- newMonopoly/src/Utilities.java
+++ newMonopoly/src/Utilities.java
@@ -1,10 +1,11 @@
 
 public class Utilities extends Property{
 	
-	public Utilities(String name, int location, int price, boolean isOwned){
+	public Utilities(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
 		this.setOwned(isOwned);
+		this.setTitledeed(titledeed);
 	}
 }
