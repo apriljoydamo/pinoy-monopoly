5be0a153259fc71a8b0cb1b19962dd5143005dcc
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-03-30 16:21:23 +0000

    same

--- trunk/newMonopoly/src/BoardBlock.java
+++ trunk/newMonopoly/src/BoardBlock.java
@@ -14,9 +14,10 @@ public class BoardBlock {
 		block[8] = new Block(new Estate("Rizal Park", 8, 130, false, new TitleDeed(6, 30, 90, 270, 400, 550, 50, 50)));			//blue
 		block[9] = new Block(new Estate("Dela Rosa St.", 9, 120, false, new TitleDeed(8, 40, 100, 300, 450, 600, 60, 50)));		//blue
 		block[10] = new Block("Jail", 10);								//jail
-		
 		block[11] = new Block(new Estate("Shopwise", 11, 140, false, new TitleDeed(10, 50, 150, 450, 625, 750, 70, 100)));			//pink
+		
 		//block[12] = new Block(new Utilities("Meralco", 12, 150, false, new TitleDeed()));		//utility
+		
 		block[13] = new Block(new Estate("SLEX", 13, 140, false, new TitleDeed(10, 50, 150, 450, 625, 750, 70, 100)));				//pink
 		block[14] = new Block(new Estate("Walter Mart", 14, 160, false, new TitleDeed(10, 60, 180, 500, 700, 900, 80, 100)));		//pink
 		block[15] = new Block(new Station("Buendia Station", 15, 200, false, new TitleDeed(25, 50, 100, 200, 100)));	//station
@@ -33,7 +34,9 @@ public class BoardBlock {
 		block[25] = new Block(new Station("Taft Ave. Station", 25, 200, false, new TitleDeed(25, 50, 100, 200, 100)));//station
 		block[26] = new Block(new Estate("EDSA", 26, 250, false, new TitleDeed(22, 110, 330, 800, 975, 1150, 130, 150)));				//yellow
 		block[27] = new Block(new Estate("Magsaysay Bridge", 27, 250, false, new TitleDeed(22, 110, 330, 800, 975, 1150, 130, 150)));	//yellow
-		block[28] = new //Block(new Utilities("Maynilad", 28, 150, false, new TitleDeed()));		//utility
+		
+		//block[28] = new Block(new Utilities("Maynilad", 28, 150, false, new TitleDeed()));		//utility
+		
 		block[29] = new Block(new Estate("Star City", 29, 260, false, new TitleDeed(24, 120, 360, 850, 1025, 1200, 140, 150)));		//yellow
 		block[30] = new Block("Go to Jail", 30);						//go to jail
 		
