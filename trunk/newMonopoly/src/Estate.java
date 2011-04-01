782576884fd3f66d7f699d1c71e4fdef4fd73a14
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-04-01 19:08:24 +0000

    added colorGroup variable

--- trunk/newMonopoly/src/Estate.java
+++ trunk/newMonopoly/src/Estate.java
@@ -1,13 +1,37 @@
 
 public class Estate extends Property{
-	//private String estateName = super.getBlockName();
-	
-	public Estate(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
+	private int numberOfHouse, colorGroup;
+    //0 brown
+    //1 blue
+    //2 pink
+    //3 orange
+    //4 red
+    //5 yellow
+    //6 green
+    //7 dark blue
+	public Estate(String name, int location, int price, int colorGroup, boolean isOwned, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
+		this.setColorGroup(colorGroup);
 		this.setOwned(isOwned);
 		this.setTitledeed(titledeed);
+	}
+
+	public void setNumberOfHouse(int numberOfHouse) {
+		this.numberOfHouse = numberOfHouse;
+	}
+
+	public int getNumberOfHouse() {
+		return numberOfHouse;
+	}
+
+	public void setColorGroup(int colorGroup) {
+		this.colorGroup = colorGroup;
+	}
+
+	public int getColorGroup() {
+		return colorGroup;
 	}
 
 }
