c042ce1c8b9eee20d1e877f46632a4508471d450
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-04-14 18:19:17 +0000

    added variable

--- trunk/newMonopoly/src/Utilities.java
+++ trunk/newMonopoly/src/Utilities.java
@@ -1,11 +1,17 @@
 
 public class Utilities extends Property{
-	
+	private boolean isMortgaged;
 	public Utilities(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
 		this.setOwned(isOwned);
 		this.setTitledeed(titledeed);
+	}
+	public void setMortgaged(boolean isMortgaged) {
+		this.isMortgaged = isMortgaged;
+	}
+	public boolean isMortgaged() {
+		return isMortgaged;
 	}
 }
