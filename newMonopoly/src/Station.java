d67fdc39fa47a8d46ae529d35d64aa46ea04dd29
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-04-14 18:19:10 +0000

    added variable

--- newMonopoly/src/Station.java
+++ newMonopoly/src/Station.java
@@ -1,11 +1,19 @@
 
 public class Station extends Property{
-	
+	private boolean isMortgaged;
 	public Station(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
 		this.setOwned(isOwned);
 		this.setTitledeed(titledeed);
+	}
+
+	public void setMortgaged(boolean isMortgaged) {
+		this.isMortgaged = isMortgaged;
+	}
+
+	public boolean isMortgaged() {
+		return isMortgaged;
 	}
 }
