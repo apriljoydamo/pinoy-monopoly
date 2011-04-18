c2f7ba96a5c64940a2ed14b3c8e3c0d45057bf11
AprilJoyDamo@gmail.com <AprilJoyDamo@gmail.com@9bd1e52a-276e-2c95-ae9f-5c9a23874dac>
2011-04-18 20:01:30 +0000

    added isMortgaged

--- trunk/newMonopoly/src/Station.java
+++ trunk/newMonopoly/src/Station.java
@@ -1,19 +1,12 @@
 
 public class Station extends Property{
-	private boolean isMortgaged;
-	public Station(String name, int location, int price, boolean isOwned, TitleDeed titledeed){
+
+	public Station(String name, int location, int price, boolean isOwned, boolean isMortgaged, TitleDeed titledeed){
 		this.setPrice(price);
 		this.setName(name);
 		this.setLocation(location);
 		this.setOwned(isOwned);
+		this.setMortgaged(isMortgaged);
 		this.setTitledeed(titledeed);
-	}
-
-	public void setMortgaged(boolean isMortgaged) {
-		this.isMortgaged = isMortgaged;
-	}
-
-	public boolean isMortgaged() {
-		return isMortgaged;
 	}
 }
